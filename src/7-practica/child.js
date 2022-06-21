98e17a652af0e5e67376f3d4832b8c9276fc0c5b
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-06-21 16:17:24 +0200

    fase final

--- src/7-practica/child.js
+++ src/7-practica/child.js
@@ -3,39 +3,42 @@ import * as fs from "fs/promises";
 console.log("child strarted working");
 
 process.on("message", (arg) => {
-  try {
-    calcular(arg.seed);
-  } catch (Error) {
-    process.send({ msg: "Error en el hijo", err: Error });
-    console.log(Error);
-  }
+  calcular(arg.seed);
 });
 
 async function calcular(seed) {
-  let texto = "" + seed;
-  let iteracion = seed;
-  const scriptFile = process.argv[1];
-  const start = process.hrtime();
+  try {
+    let parse = parseInt(seed);
+    let texto = "" + seed;
+    let iteracion = seed;
+    const scriptFile = process.argv[1];
+    const start = process.hrtime();
+    if (!parse) {
+      throw new Error("no es un numero");
+    }
+    while (iteracion !== 1) {
+      if (iteracion < 1) {
+        texto = "error";
+        break;
+      }
+      if (iteracion % 2 === 0) {
+        iteracion = iteracion / 2;
+        texto += " " + iteracion;
+      } else {
+        iteracion = iteracion * 3 + 1;
+        texto += " " + iteracion;
+      }
+    }
+    const end = process.hrtime();
+    texto += " Fecha inicio: " + start[1];
+    texto += " Fecha fin: " + end[1];
+    texto += " Tiemo transcurrido: " + (end[1] - start[1]);
 
-  while (iteracion !== 1) {
-    if (iteracion < 1) {
-      texto = "error";
-      break;
-    }
-    if (iteracion % 2 === 0) {
-      iteracion = iteracion / 2;
-      texto += " " + iteracion;
-    } else {
-      iteracion = iteracion * 3 + 1;
-      texto += " " + iteracion;
-    }
+    await fs.writeFile(`${scriptFile}.${seed}.txt`, texto);
+    process.exit();
+  } catch (Error) {
+    console.log(Error + "Hola estoy aqui");
+    process.send({ msg: "Error en el hijo", err: Error, seed: seed });
+    process.exit();
   }
-  const end = process.hrtime();
-  texto += " Fecha inicio: " + start[1];
-  texto += " Fecha fin: " + end[1];
-  texto += " Tiemo transcurrido: " + (end[1] - start[1]);
-
-  await fs.writeFile(`${scriptFile}.${seed}.txt`, texto);
-
-  process.exit();
 }
--- src/7-practica/index.js
+++ src/7-practica/index.js
@@ -1,12 +1,11 @@
 import * as cp from "child_process";
 
-const arr = [0, 1, 2, 3, 5, 7, 11, 13, 3424243523423424];
+const arr = [["@sadfsafa"], 0, 1, 2, 3, 5, 7, 11, 13, 3424243523423424];
 
 arr.forEach((seed) => {
   let child = cp.fork("./src/7-practica/child.js");
   child.send({ seed: seed });
   child.on("message", (arg) => {
-    console.log(`Recived from child ${arg.msg}`);
-    process.exit();
+    console.log(`Recived from child ${arg.msg} en el ${arg.seed} y el Error es ${arg.err}`);
   });
 });
