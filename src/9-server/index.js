fb117b1cd0c15ca6d1ffd386cad5f39be3109390
Bruno Lopez <bruno.lopez.rodriguez.es@gmail.com>
2022-06-23 10:21:28 +0200

    refactor server

--- src/9-server/index.js
+++ src/9-server/index.js
@@ -1,42 +1,46 @@
 import * as http from "http";
-import * as fs from "fs";
+import * as fs from "fs/promises";
 
 const PORT = 8000;
-const BASE_URL = "C:/Users/IGM000108/Desktop/igmlabs/blr-node-astro-bookings/src";
+// const BASE_URL = "C:/Users/IGM000108/Desktop/igmlabs/blr-node-astro-bookings/src";
+const BASE_URL = "./src";
+// const BASE_URL = __dirname;
 const OK = 200;
 const NOT_FOUND = 404;
 const server = http.createServer();
 
-server.on("request", async (requestStream, responseStream) => {
+server.on("request", processRequest);
+
+async function processRequest(requestStream, responseStream) {
   let url = BASE_URL + requestStream.url;
-  console.log(requestStream.url);
-  await fs.readFile(url, (err, fileContent) => {
-    processRequest(err, fileContent, requestStream, responseStream);
-    if (!err) {
-      writePipe(fileContent, requestStream.url);
-    }
-  });
-});
+  try {
+    const fileContent = await fs.readFile(url);
+    writeResponseOk(fileContent);
+    await writePipe({ fileContent, url: requestStream.url });
+  } catch (error) {
+    writeResponseError(error);
+  } finally {
+    responseStream.end();
+  }
+
+  function writeResponseError(error) {
+    responseStream.writeHead(NOT_FOUND, { "Content-type": "text/plain" });
+    responseStream.write(buildMessageNotOk(requestStream.url, error));
+  }
+
+  function writeResponseOk(fileContent) {
+    responseStream.writeHead(OK, { "Content-type": "text/plain" });
+    responseStream.write(buildMessageOk(requestStream.url, fileContent));
+  }
+}
 
 server.listen(PORT, () => {
   console.log("server listening");
 });
 
-function processRequest(err, fileContent, requestStream, responseStream) {
-  if (err) {
-    responseStream.writeHead(NOT_FOUND, { "Content-type": "text/plain" });
-    responseStream.write(buildMessageNotOk(requestStream.url, err));
-    responseStream.end();
-  } else {
-    responseStream.writeHead(OK, { "Content-type": "text/plain" });
-    responseStream.write(buildMessageOk(requestStream.url, fileContent));
-    responseStream.end();
-  }
-}
-
 function buildMessageOk(url, fileContent) {
   let message = "";
-  message += "Hola mundo \n";
+  message += "Arquive Found:  \n";
   message += url;
   message += "\n";
   message += fileContent;
@@ -52,10 +56,14 @@ function buildMessageNotOk(url, err) {
   return message;
 }
 
-function writePipe(fileContent, url) {
-  let archiveName = url.split("/");
+async function writePipe(urlContent) {
+  if (!urlContent.url || !urlContent.fileContent) {
+    throw new Error("No url or no data");
+  }
+  let archiveName = urlContent.url.split("/");
   let finalArchiveName = archiveName.join(".");
-  let writeStream = fs.createWriteStream("./src/9-server/" + finalArchiveName);
-  writeStream.write(fileContent);
-  writeStream.close();
+  await fs.writeFile(finalArchiveName, urlContent.fileContent);
+  //   let writeStream = fs.createWriteStream("./src/9-server/" + finalArchiveName);
+  //   writeStream.write(fileContent);
+  //   writeStream.close();
 }
